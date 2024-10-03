66341694675d44c95025c1f5f7dd9a8ddfca57f4
mattswer31 <songma418@gmail.com>
2024-10-03 01:44:40 -0400

    Assignment 1

---
+++ python_syntax.py
@@ -0,0 +1,37 @@
+# 1. Showcase Your Dance Moves!
+# Task 1: Code Correction
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2: Your Mood Today
+mood = input("How are you doing today? Are you happy or sad?")
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
+else:
+    print("Please limit your response to happy or sad.")
+
+# 2. Python Naming Convention Practice
+# Task 1: Code Correction
+PI_VALUE = 3.14
+MAX_LIMIT = 1000
+user_age = 25
+user_location = "New York"
+
+# 3. Arithmetic Operations in Daily Life
+# Task 1: Grocery Store Math
+bread = 3.17
+peanut_butter = 6.52
+jelly = 7.55
+pbj_sandwich = bread + peanut_butter + jelly
+print("The total cost of bread, peanut butter, and jelly is $" + str(pbj_sandwich))
+
+# Task 2: Bank Interest
+savings = 10000.00
+interest_rate = .07
+savings += savings*interest_rate
+print("After a year your savings is now at $" + str(savings))
